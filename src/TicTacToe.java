0a2d7fde490f72d5634abb7a496aadf09903f2b1
WaseemAkhtar032 <waseemakhtar032@gmail.com>
2022-11-28 09:07:43 +0530

    Added: Welcome to the TicTacToe using OOPS!

---
+++ src/TicTacToe.java
@@ -0,0 +1,13 @@
+public class TicTacToe extends gamechooseturn.TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Welcome to TicTacToe Game!");
+
+            //Initialize the object
+            TicTacToe obj = new TicTacToe();
+            //Starting the game.
+            obj.startGame();
+            //For playing again
+            obj.playAgain();
+
+        }
+    }
